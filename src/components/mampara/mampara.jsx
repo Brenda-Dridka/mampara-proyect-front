b90cd23184c74e7a8331c499422cbaf3e6fb5af7
Brenda-Dridka <ruiz83636@gmail.com>
2023-12-06 18:11:34 -0600

    implementacion de el guardado de las etiquetas

--- src/components/mampara/mampara.jsx
+++ src/components/mampara/mampara.jsx
@@ -7,14 +7,17 @@ import BotonOption from "../global/botonOptions";
 import "../../style/global/global.css";
 import AgregarProducto from "../productos/formulario";
 
+const apiUrlEtiquetas = "http://localhost:3000/api/v1/etiquetas";
+const apiUrlEtiquetasExt54_2 = "http://localhost:3000/api/v1/etiquetasExt54_2";
+
 export default function Component2() {
   const [labelColor, setLabelColor] = React.useState("#ffffff");
   const [etiquetasAgregadas, setEtiquetasAgregadas] = useState([]);
-  const [etiquetasPorExtrusor, setEtiquetasPorExtrusor] = useState({});
   const [ext54lletiquetas, setExt54lletiquetas] = useState([]);
   const [etiquetasBussl, setEtiquetasBussl] = useState([]);
   const [etiquetasExt70_2, setEtiquetasExt70_2] = useState([]);
-  const [etiquetaSeleccionada, setEtiquetaSeleccionada] = useState(null);
+
+  /*   const [etiquetaSeleccionada, setEtiquetaSeleccionada] = useState(null); */
 
   useEffect(() => {
     axios
@@ -29,6 +32,20 @@ export default function Component2() {
         console.error("Error al cargar etiquetasExt54ll:", error);
       });
   }, []);
+
+  /* useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/v1/etiquetasExt54_2")
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error("No se pudieron cargar los etiquetasExt54ll.");
+        }
+        setExt54lletiquetas(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al cargar etiquetasExt54ll:", error);
+      });
+  }, [pageReloaded]); */
 
   useEffect(() => {
     axios
@@ -85,49 +102,25 @@ export default function Component2() {
     return formattedDate;
   };
 
-  /* useEffect(() => {
+  useEffect(() => {
     console.log(ext54lletiquetas);
-    axios
-      .post("http://localhost:3000/api/v1/etiquetasExt54_2", {
-        data: ext54lletiquetas,
-      })
-      .then((response) => {
-        // Aquí puedes manejar la respuesta de la API
-        console.log("Etiqueta guardada con éxito", response.data);
-        handleClose();
-      })
-      .catch((error) => {
-        // Manejar errores
-        console.error("Error al guardar la etiqueta", error);
-      });
-  }, [ext54lletiquetas]); */
-  /*   useEffect(() => {
-    console.log(ext54lletiquetas);
-    const handleEtiquetasExt54Guardado = (etiquetas) => {
-      const etiquetasConExtrusorPosicion = etiquetas.map((item, index) => ({
-        ...item,
-        extrusor: "EXT54-II",
-        posicion: index + 1,
-      }));
 
-      axios
-        .post("http://localhost:3000/api/v1/etiquetasExt54_2", {
-          data: etiquetasConExtrusorPosicion,
-        })
-        .then((response) => {
-          console.log(
-            "Etiquetas guardadas en etiquetasExt54_2:",
-            response.data
-          );
-        })
-        .catch((error) => {
-          console.error(
-            "Error al guardar las etiquetas en etiquetasExt54_2:",
-            error
-          );
-        });
+    // Aquí manejas el guardado masivo al cambiar ext54lletiquetas
+    const guardarEtiquetasMasivo = async () => {
+      try {
+        // Guarda las nuevas etiquetas
+
+        await axios.post(apiUrlEtiquetasExt54_2, ext54lletiquetas);
+
+        console.log("Etiquetas guardadas con éxito");
+      } catch (error) {
+        console.error("Error al guardar las etiquetas", error);
+      }
     };
-  }, [ext54lletiquetas]); */
+
+    // Llama a la función de guardado masivo cuando cambia ext54lletiquetas
+    guardarEtiquetasMasivo();
+  }, [ext54lletiquetas]);
 
   return (
     <div style={{ display: "flex", gap: "2rem", flexDirection: "column" }}>
@@ -198,7 +191,6 @@ export default function Component2() {
                   setList={setExt54lletiquetas}
                   group="shared-group-name"
                   className="position"
-                  onEnd={() => handleEtiquetasExt54Guardado(ext54lletiquetas)}
                 >
                   {ext54lletiquetas.map((item) => (
                     <div
@@ -302,10 +294,3 @@ export default function Component2() {
     </div>
   );
 }
-
-/* 
-como puedo implementar el guardado de etiquetas en el siguiente codigo
-se nesesota guardar las etiquetas que ya se estan se están consumiendo desde la api "http://localhost:3000/api/v1/etiquetas",guardarlas a la api "http://localhost:3000/api/v1/etiquetasExt54_2",
- al colocarlas en la seccion del extrusor setExt54lletiquetas, se haga un guardado automatico, y la data que tiene de la api etiquetas, se pase a la api de etiquetasExt54_2, adicionandole 
- los datos de "extrusor" y "posicion", para el campo extrusor se guardara nombre del value="EXT54-II", y en el campo posicion se guardara el numero de la pocicion que muesta el array de datos cuando la etiqueta cambia de lugar
-*/
